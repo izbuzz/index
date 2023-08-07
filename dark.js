4e52694facefb1ae2a34753e59806c33b4f65dd2
izbuzz <ivzhang@stu.allstarcode.org>
2023-08-07 12:50:44 -0400

    Add icons for darkmode toggle

--- dark.js
+++ dark.js
@@ -1,9 +1,12 @@
+// get the user's preferences, may be null 
+// if they have not toggled a theme yet
 const theme = localStorage.getItem("theme");
 
+// toggle scheme based on user choice
 if (theme === "dark") {
   document.documentElement.classList.toggle("dark-mode");
 } else if (theme === "light") {
   document.documentElement.classList.toggle("light-mode");
 }
-// if currentTheme is null, the theme will be chosen based on
+// if theme is null, the theme will be chosen based on
 // the user's OS preferences via prefers-color-scheme
